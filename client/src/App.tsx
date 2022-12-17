6fef2f3a7c8cb9e0baf0f0350ff0c834b7917240
Isaac Williams <isaaciyaye174@gmail.com>
2022-12-17 14:38:06 +0100

    New Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import "./App.css";
-
+import "./App.scss";
+import { Route, Routes } from "react-router-dom";
 function App() {
 	const [text, setText] = useState("");
 
-	return <div className="App">grey</div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/login" element={<h1></h1>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
